571b1c074ca3863ad769e8c8fdee003c4e5890d4
Claire <clairelogan16@gmail.com>
2021-07-06 10:38:51 -0700

    updates to js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-
-
-//adding new shows once form submits
+//Adding new card once form submits
 
 document.querySelector('#libraryForm').addEventListener('submit', (e) => {
+    //preventing auto refresh of page on submit
     e.preventDefault();
-    console.log("event triggered")
+    //console.log("event triggered")
 
     let newCard = {
         title: e.target.title.value,
@@ -17,29 +16,50 @@ document.querySelector('#libraryForm').addEventListener('submit', (e) => {
         image_url: e.target.image_url.value
     }
 
+    //reseting the form so previous answers are not saved/appear in dropdown
     document.querySelector('#libraryForm').reset();
 
+    //calling the render card function
     renderLibraryCard(newCard)
-
-    //adding button functionality
-    document.querySelectorAll("#library .addButton").forEach(btn => {
-        btn.addEventListener('click', (e) => {
-            btn.textContent = '-'
-            //btn.className = "removeButton"
-            let card = btn.closest("li")
-            document.querySelector('#watchListUl').append(card)
-                btn.addEventListener('click', (e) => {
-                    if (confirm("Are you sure you want to remove this from your Watch List?")){
-                        document.querySelector("#libraryList").append(card)
-                        btn.textContent = '+'
-                    }
-                })
-        })
-    })
-
 })
 
-//Aside Render
+//function to remove placeholder and then remove listener after the first occurence 
+function removePlaceholder(){
+    document.querySelector('#placeholder').remove();
+    removeEventListener('click', removePlaceholder);
+}
+
+//Add button click functionality - link to article that showed me how to do this: https://medium.com/swlh/now-you-see-it-now-you-dont-two-ways-to-toggle-using-vanilla-javascript-9f45c6db7698 
+
+function addButtonFunctionality(buttonName, cardName) {
+
+    buttonName.addEventListener('click', (e) => {
+
+        inWL = !inWL
+    
+        if(inWL === true) {
+            document.querySelector("#watchListUl").append(cardName);
+            //document.querySelector('#placeholder').remove();
+        } else { document.querySelector("#libraryList").append(cardName);
+        }
+        buttonName.textContent = buttonStates[buttonName.textContent]
+    })
+
+    let inWL = false
+
+    //so we can toggle the content inside the button
+    let buttonStates = {
+        "+" : "-",
+        "-" : "+"
+    }
+
+    //calling the event listener to trigger the remove Placeholder function -- need to figure out how to have this only run once
+    buttonName.addEventListener('click', removePlaceholder)
+
+ 
+}
+
+//Aside Render - create the lefthand menu with username / profile picture
 
 function asideRender() {
     let profilePic = document.querySelector('#profilePicture')
@@ -49,6 +69,8 @@ function asideRender() {
     profilePic.src = allData.profilePicture
     userName.textContent = allData.username
 }
+
+//Card Render - render each card based on data.js
 
 function renderLibraryCard(card){
     //creating elements
@@ -75,9 +97,10 @@ function renderLibraryCard(card){
     li.append(img, h3Title, pYear, pEpisodes, pStreaming, cardButton);
     document.querySelector("#libraryList").append(li)
 
+    //call funtion to add event listening to cardButton
+    addButtonFunctionality(cardButton, li);
 
 }
-
 
 
 //Initial Render
@@ -85,66 +108,14 @@ function renderLibraryCard(card){
 function initialRender() {
     //console.log(allData);
     asideRender();
+    //call function to render each card, pulling each object from array
     allData.library.forEach(renderLibraryCard);
 
-
-  
-
-    // //Event listener for adding card from Library to Watch List
-
-    document.querySelectorAll("#library .addButton").forEach(btn => {
-        btn.addEventListener('click', (e) => {
-            btn.textContent = '-'
-            //btn.className = "removeButton"
-            let card = btn.closest("li")
-            document.querySelector('#watchListUl').append(card)
-                btn.addEventListener('click', (e) => {
-                    if (confirm("Are you sure you want to remove this from your Watch List?")){
-                        document.querySelector("#libraryList").append(card)
-                        btn.textContent = '+'
-                    }
-                })
-           document.querySelector('#placeholder').remove()
-        })
-    })
-
-
-    //Hover over card and get bold black background  - couldn't get to work
-    document.querySelectorAll('#library li .card').forEach(card => {
-        card.addEventListener('mouseover', (e) => {
-            card.style.borderColor = 'red';
-            card.style.borderWidth = '10px';
-        })
-    })
-
-
-  
-    //If (-) at top of Watch List or Library div is clicked, minimizes (or toggles Ul) + changes the (-) to a (+)
-    //If (+) aat top of Watch List or Library div is clicked, maximizes (or toggles Ul) + changes the (+) to a (-)
-    //WL minimizer
-    // const wLminMaxBtn = document.querySelector('#wLminMaxButton')
-    // wLminMaxBtn.addEventListener('click', (e) => {
-    //     wLminMaxBtn.textContent = '+'
-    //     document.querySelector('#watchListUl').style.maxHeight = 0;
-
-    //         wLminMaxBtn.addEventListener('click', (e) => {
-    //             document.querySelector('#watchListUl').style.maxHeight = "600 px";
-    //             wLminMaxBtn.textContent = '-';
-
-    //         })
-
-
-    // })
-
-    // //Lib minimizer
-    // const libminMaxButton = document.querySelector('#libminMaxButton')
-    // libminMaxButton.addEventListener('click', (e) => {
-    //     libminMaxButton.textContent = '+'
-    //     document.querySelector('#libraryList').style.height = 0;
-    // })
-
 }
-initialRender()
+
+initialRender();
+
+
 
 
 
@@ -152,6 +123,23 @@ initialRender()
 
 
 //old notes / code to Delete
+
+
+//new button functionality - old code
+// function addButtonFunctionality(buttonName, cardName) {
+//     buttonName.addEventListener('click', (e) => {
+//         buttonName.textContent = '-'
+//             //btn.className = "removeButton"
+//         document.querySelector('#watchListUl').append(cardName)
+//             buttonName.addEventListener('click', (e) => {
+//                     if (confirm("Are you sure you want to remove this from your Watch List?")){
+//                         document.querySelector("#libraryList").append(cardName)
+//                         buttonName.textContent = '+'
+//                     }
+//                 })
+//             document.querySelector('#placeholder').remove()
+//         })
+//     }
 
 //move card to watchlist when "+" is clicked ; once in Watch List + --> -; if - is clicked, goes back down to library
     //could play with adding an "archived section"
@@ -193,3 +181,49 @@ initialRender()
     //     li.append(pText);
     //     document.querySelector('#watchListUl').append(li);
     // } 
+
+
+
+    // //Function to add listener for adding card from Library to Watch List
+// function buttonEventListen() {
+//     document.querySelectorAll("#library .addButton").forEach(btn => {
+//         btn.addEventListener('click', (e) => {
+//             btn.textContent = '-'
+//             //btn.className = "removeButton"
+//             let card = btn.closest("li")
+//             document.querySelector('#watchListUl').append(card)
+//                 btn.addEventListener('click', (e) => {
+//                     if (confirm("Are you sure you want to remove this from your Watch List?")){
+//                         document.querySelector("#libraryList").append(card)
+//                         btn.textContent = '+'
+//                     }
+//                 })
+//             document.querySelector('#placeholder').remove()
+//         })
+//     })
+// }
+
+
+    //If (-) at top of Watch List or Library div is clicked, minimizes (or toggles Ul) + changes the (-) to a (+)
+    //If (+) aat top of Watch List or Library div is clicked, maximizes (or toggles Ul) + changes the (+) to a (-)
+    //WL minimizer
+    // const wLminMaxBtn = document.querySelector('#wLminMaxButton')
+    // wLminMaxBtn.addEventListener('click', (e) => {
+    //     wLminMaxBtn.textContent = '+'
+    //     document.querySelector('#watchListUl').style.maxHeight = 0;
+
+    //         wLminMaxBtn.addEventListener('click', (e) => {
+    //             document.querySelector('#watchListUl').style.maxHeight = "600 px";
+    //             wLminMaxBtn.textContent = '-';
+
+    //         })
+
+
+    // })
+
+    // //Lib minimizer
+    // const libminMaxButton = document.querySelector('#libminMaxButton')
+    // libminMaxButton.addEventListener('click', (e) => {
+    //     libminMaxButton.textContent = '+'
+    //     document.querySelector('#libraryList').style.height = 0;
+    // })
